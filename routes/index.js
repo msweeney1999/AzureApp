2f20e3181de1fe30ac361fa2d4b12ef42613e86e
mark <mark@nowhere.com>
2018-03-30 15:03:31 -0400

    adding images

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,37 @@
 var express = require('express');
+var { MongoClient } = require('mongodb');
+
 var router = express.Router();
+
 const title = process.env.TITLE;
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title });
+router.get('/', function (req, res, next) {
+    const url = process.env.URL;
+    const password = process.env.PASS;
+    const user = process.env.USER;
+    const dbName = 'library';
+    console.log('URL', url);
+    (async function mongo() {
+        let client;
+        try {
+            client = await MongoClient.connect(url, {
+                auth: {
+                    user,
+                    password
+                }
+            }
+            );
+
+            const db = client.db(dbName);
+            const books = await db.collection('books').find().toArray();
+            console.log('books', books);
+            res.render('index',
+                { books });
+        } catch (err) {
+            console.log(err);
+        }
+    }())
 });
 
 module.exports = router;
-
